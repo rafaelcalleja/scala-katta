31bcf93143e9e10709976453cd8f96a3f40a54d2
Rafael Calleja <rafaelcalleja@gmail.com>
2019-04-23 11:08:27 +0200

    improve abstraction

--- src/main/scala/essential/Calculator.scala
+++ src/main/scala/essential/Calculator.scala
@@ -10,38 +10,39 @@ import scala.math._
   * A Number has a value of type Double .
   */
 sealed trait Expression {
+  def operation(left: Expression, right: Expression, f: (Double, Double) => Result): Result = (left.eval, right.eval) match {
+      case (Success(a), Success(b)) => f(a, b)
+      case (Failure(a), _) => Failure(a)
+      case (_, Failure(a)) => Failure(a)
+  }
+
   def eval: Result = this match {
     case Number(n) => Success(n)
-    case Addition(left, right) => (left.eval, right.eval) match {
-      case (Success(a), Success(b)) => Success(a.toDouble + b.toDouble)
-      case (_, Failure(a)) => Failure(a.toString)
-      case (Failure(a), _) => Failure(a.toString)
-    }
-    case Subtraction(left, right) => (left.eval, right.eval) match {
-      case (Success(a), Success(b)) => Success(a - b)
-      case (_, Failure(a)) => Failure(a.toString)
-      case (Failure(a), _) => Failure(a.toString)
+    case Addition(left, right) => operation(left, right, (a, b) => Success(a + b))
+    case Subtraction(left, right) => operation(left, right, (a, b) => Success(a - b))
+
+    case Division(left, right) => (left, right) match {
+      case (l, r) => operation(l, r, (a, b) =>
+        if (b == 0.0)
+          Failure("Division by zero")
+        else Success(a / b)
+      )
     }
 
-    case Division(left, right) => (left.eval, right.eval) match {
-      case (_, Success(0.0)) => Failure("Division by zero")
-      case (Success(a), Success(b)) => Success(a.toDouble / b.toDouble)
-    }
-
-    case SquareRoot(number) => number.eval.result.toDouble match {
-      case a if a < 0 => Failure("Square root of negative number")
-      case a => Success(sqrt(a))
+    case SquareRoot(number) => number.eval match {
+      case Success(a) if a < 0 => Failure("Square root of negative number")
+      case Success(a) => Success(sqrt(a))
     }
   }
 }
 
-case class Addition(left: Expression, right: Expression) extends Expression
-case class Subtraction(left: Expression, right: Expression) extends Expression
-case class Number(value: Double) extends Expression //base case
+final case class Addition(left: Expression, right: Expression) extends Expression
+final case class Subtraction(left: Expression, right: Expression) extends Expression
+final case class Number(value: Double) extends Expression //base case
 
 
-case class Division(left: Expression, right: Expression) extends Expression
-case class SquareRoot(number: Expression) extends Expression
+final case class Division(left: Expression, right: Expression) extends Expression
+final case class SquareRoot(number: Expression) extends Expression
 
 sealed trait Result {
   def result: String = this match {
