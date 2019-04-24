3a52d9c02452b08fe1c9687aab04a042cd8a8ed7
Rafael Calleja <rafaelcalleja@gmail.com>
2019-04-24 17:29:52 +0200

    add type parameter

--- src/main/scala/essential/Sequence/IntList.scala
+++ src/main/scala/essential/Sequence/IntList.scala
@@ -1,20 +1,35 @@
 package essential.Sequence
 
-sealed trait IntList {
+import scala.language.implicitConversions
+
+/**
+  * Fold Pattern
+  *
+  * For an algebraic datatype A, fold converts it to a generic type B . Fold is a structural recursion with:
+  * • one func on parameter for each case in A ;
+  * • each func on takes as parameters the fields for its associated class;
+  * • if A is recursive, any func on parameters that refer to a recursive field take a parameter of type B .
+  *
+  * The right-hand side of pattern matching cases, or the polymorphic methods as appropriate, consists of calls to the appropriate func on.
+  */
+sealed trait IntList[B] {
   def length: Int = fold[Int](0, (_, tail) => 1 + tail )
 
-  def double: IntList = fold[IntList](End, (that, tail) => Pair(that * 2, tail) )
+  def double: IntList[B] = fold[IntList[B]](End(), (that, tail) => Pair[B](that * 2, tail) )
 
   def product: Int = fold[Int](1, (that, next) => that * next)
 
   def sum: Int = fold[Int](0, (that, next) => that + next)
 
-  def fold[A](end: A, f: (Int, A) => A): A =
+  implicit def BToInt(value: B): Int = value.toString.toInt
+  implicit def IntToB(value: Int): B = value.asInstanceOf[B]
+
+  def fold[A](end: A, f: (B, A) => A): A =
     this match {
-      case End => end
+      case End() => end
       case Pair(hd, tl) => f(hd, tl.fold(end, f))
     }
 }
 
-case object End extends IntList
-case class Pair(head: Int, tail: IntList) extends IntList
+case class End[B]() extends IntList[B]
+case class Pair[B](head: B, tail: IntList[B]) extends IntList[B]
